7e524b2509fba367de9e730314359e15773d35b3
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-09-01 15:29:38 -0700

    defined a user schema and compiled a User model in User.js

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/**
+ * References:
+ * {@link https://stackoverflow.com/questions/11101955/mongodb-schema-design-for-multiple-auth-user-accounts}
+ * {@link https://stackoverflow.com/questions/42608919/mongoose-user-model-for-handling-local-and-social-auth-providers}
+ * {@link http://www.passportjs.org/docs/profile/}
+ */
+
+const internalAccountSchema = new Schema({
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const externalAccountSchema = new Schema({
+  provider: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+});
+
+// Define the user schema
+const userSchema = new Schema({
+  // _id holds the user's email
+  _id: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: Stirng,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  // Single nested subdocument
+  internalAuth: internalAccountSchema,
+  // Single nested subdocument
+  externalAuth: externalAccountSchema,
+  picture: {
+    type: String,
+    require: true,
+  },
+  joined: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Convert the userSchema into a model we can work with
+mongoose.model("User", userSchema);
