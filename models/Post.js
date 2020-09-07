5e185135709eac38622ed20a8bdcdd14fd4d8aeb
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-09-06 23:35:42 -0700

    defined a post schema and compiled a Post model in Post.js

---
+++ models/Post.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+/**
+ * Define the post schema
+ *
+ * TODO: When a user goes to delete their account, ask whether they want their comments and posts deleted as well.
+ *
+ */
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Convert the postSchema into a model we can work with
+mongoose.model("Post", postSchema);
