fb6dd010d0b3c289cb1f6b14e5795e5a0df146ea
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-09-17 23:58:44 -0700

    implemented redux-persist in the client
    ensures that state in the redux store isn't lost if the user refreshes the page

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import store from "./store.js";
+import { store, persistor } from "./store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import authReducer from "./components/auth/authSlice.js";
 
-export default configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+let persistor = persistStore(store);
+
+export { store, persistor };
