dcb0dd85caf1e144abc34eb547fe3090c9e6df48
d4y.dr34mzzz <d4y.dr34mzzz@gmail.com>
2020-12-22 21:36:27 -0800

    [client] updated the Explore component to include pagination
    - users need to be sorted alphabetically by name for the default pagination
    - the explore page will be made responsive in an upcoming commit
    - advanced search functionality needs to be added in a later commit
    - start thinking about a testing suite and testing environment

--- client/src/components/explore/Explore.js
+++ client/src/components/explore/Explore.js
@@ -3,34 +3,109 @@ import { connect } from "react-redux";
 import { fetchProfiles } from "../profile/profileSlice.js";
 import LoadingIcon from "../shared/LoadingIcon.js";
 import ProfileSummaryCard from "./ProfileSummaryCard.js";
+import ReactPaginate from "react-paginate";
 
 class Explore extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      limit: 30,
+      pageCount: null,
+    };
+
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
 
   componentDidMount() {
-    this.props.fetchProfiles();
+    const paginationData = {
+      page: 1,
+      limit: this.state.limit,
+    };
+    this.props.fetchProfiles(paginationData).then(() => {
+      this.setState({
+        pageCount:
+          this.props.profile &&
+          this.props.profile.profiles &&
+          this.props.profile.profiles.totalPages,
+      });
+    });
+  }
+
+  handlePageClick(event) {
+    const paginationData = {
+      page: event.selected + 1,
+      limit: this.state.limit,
+    };
+    this.props.fetchProfiles(paginationData).then(() => {
+      this.setState({
+        pageCount:
+          this.props.profile &&
+          this.props.profile.profiles &&
+          this.props.profile.profiles.totalPages,
+      });
+    });
   }
 
   render() {
     const { profiles } = this.props.profile;
 
+    let exceptions;
     let exploreContent;
 
-    if (!profiles || this.props.profile.fetch_profiles_status !== "succeeded") {
+    if (!profiles || this.props.profile.fetch_profiles_status === "loading") {
       exploreContent = <LoadingIcon />;
+    } else if (this.props.profile.fetch_profiles_status === "failed") {
+      exceptions = (
+        <div class="alert alert-secondary" role="alert">
+          {(() => {
+            if (
+              this.props.profile &&
+              this.props.profile.fetch_profiles_errors &&
+              this.props.profile.fetch_profiles_errors.pagination
+            ) {
+              return this.props.profile.fetch_profiles_errors.pagination.msg;
+            } else {
+              return "There was an issue processing the request. Please try again later.";
+            }
+          })()}
+        </div>
+      );
     } else {
-      exploreContent = profiles.map((profile) => (
+      exploreContent = profiles.response.map((profile) => (
         <ProfileSummaryCard key={profile._id} profile={profile} />
       ));
     }
 
     return (
       <div className="explore">
-        <div className="container-fluid explore__header p-0"></div>
+        <div className="container-fluid explore__header p-0">
+          <div className="container pt-5">{exceptions}</div>
+        </div>
         <div className="container-fluid explore__body">
-          <div className="container">{exploreContent}</div>
+          <div className="container">
+            {exploreContent}
+            <div className="react-paginate">
+              <ReactPaginate
+                pageCount={this.state.pageCount}
+                pageRangeDisplayed={7}
+                marginPagesDisplayed={1}
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                breakLabel={"..."}
+                breakClassName={"page-item"}
+                breakLinkClassName={"page-link"}
+                onPageChange={this.handlePageClick}
+                containerClassName={"pagination justify-content-center"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                activeClassName={"active"}
+                previousClassName={"page-item"}
+                previousLinkClassName={"page-link"}
+                nextClassName={"page-item"}
+                nextLinkClassName={"page-link"}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
